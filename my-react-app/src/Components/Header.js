9f6ab74a41ad8f4b411d3bcfec8911fe0621ecdf
Olena <7890572@ukr.net>
2023-03-29 08:50:49 +0200

    Added a folder src

--- my-react-app/src/Components/Header.js
+++ my-react-app/src/Components/Header.js
@@ -1,18 +1,18 @@
 import { Component } from "react";
 
 class Header extends Component {
-    render() {
-        return(
-            <header className="header">
-                <ul className='header_nav'>
-                    <li><a href='#'>LOGO</a></li>
-                    <li><a href='#'>Menu</a></li>
-                    <li><a href='#'>Сontact center</a></li>
-                    <li><a href='#'>Feedback</a></li>
-                </ul>
-            </header>
-            )
-        };
-    }
+  render() {
+    return(
+      <header className="header">
+        <ul className='header_nav'>
+          <li><a href='#'>LOGO</a></li>
+          <li><a href='#'>Menu</a></li>
+          <li><a href='#'>Сontact center</a></li>
+          <li><a href='#'>Feedback</a></li>
+        </ul>
+      </header>
+    )
+  };
+}
   
-  export default Header;
+export default Header;
